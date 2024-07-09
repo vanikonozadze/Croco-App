264a3c7ea122419bde30f4b95b4fe87e5cccc408
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2024-07-09 16:08:52 +0400

    built navigation menu for user to see users and posts

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CrocoService } from '../../service/croco.service';
 
 @Component({
   selector: 'app-navigation',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+
+  constructor(private crocoService: CrocoService) {}
+
+  toggleFavView() {
+    this.crocoService.toggleMenu();
+  }
+
+  handleSidebarClick(event: MouseEvent) {
+    if (!(event.target as HTMLElement).closest('button')) {
+      event.stopPropagation();
+    }
+  }
 
 }
