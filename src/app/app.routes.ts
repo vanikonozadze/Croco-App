b0adab58c415c1328208679765fbd2573af335ad
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2024-07-10 18:37:29 +0400

    change in the route file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('./components/main/main.component').then(mod => mod.MainComponent)},
+    { path: '', loadComponent: () => import('./components/users-table/users-table.component').then(mod => mod.UsersTableComponent)},
     { path: 'users', loadComponent: () => import('./components/users-table/users-table.component').then(mod => mod.UsersTableComponent)},
     { path: 'posts', loadComponent: () => import('./components/posts-table/posts-table.component').then(mod => mod.PostsTableComponent)},
     { path: 'users/posts/:userId', loadComponent: () => import('./components/user-posts/user-posts.component').then(mod => mod.UserPostsComponent)},
